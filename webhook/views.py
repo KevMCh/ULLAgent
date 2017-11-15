d707feca17007bcfcf8673f75a24695aa78ef1bd
Kevin Martín Chinea <alu0100774665@ull.edu.es>
2017-11-15 09:31:35 +0000

    API access

--- webhook/views.py
+++ webhook/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+import json
 
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Webhook to NewsAgent.")
+    
+def news(request):
+    response_data = {}
+    response_data['speech'] = 'This is the news.'
+    response_data['displayText'] = 'This is the news'
+
+    return HttpResponse(
+        json.dumps(response_data),
+        content_type="application/json"
+    )
