22d1940955edc252ffad964d2a3d0e544b15ba86
Kevin Martín Chinea <alu0100774665@ull.edu.es>
2017-11-28 10:54:34 +0000

    Requests to the events API

--- webhook/views.py
+++ webhook/views.py
@@ -15,12 +15,16 @@ CALL_API = CALL + WORDPRESS_API + 'convocatorias'
 NEWS = 'https://www.ull.es/portal/noticias/'
 NEWS_API = NEWS + WORDPRESS_API + POSTS
 
+EVENTS = 'https://www.ull.es/portal/agenda'
+EVENTS_API = EVENTS + '/wp-json/tribe_events/v2/events/'
+
 TOTAL_PAGE = 2
 
 def getData(action, request):
     return {
         'showNews': showNews(request),
         'showCalls': showCalls(request),
+        'showEvents': showEvents(request),
     }[action]
     
 def createList(values):
@@ -44,8 +48,23 @@ def bodyToJSON(requestBody):
     
     return json.loads(bodyUnicode)
     
+def verifyTheContextAttribute(contexts, attribute):
+    try:
+        contexts['parameters'][attribute]
+    except KeyError:
+        contexts['parameters'][attribute] = ""
+    
 def getBodyContext(body):
-    return body['result']['contexts'][0]
+    contexts = body['result']['contexts'][0]
+    
+    verifyTheContextAttribute(contexts, 'numberNews')
+    verifyTheContextAttribute(contexts, 'numberCalls')
+    verifyTheContextAttribute(contexts, 'numberEvents')
+    verifyTheContextAttribute(contexts, 'category')
+    verifyTheContextAttribute(contexts, 'dateStart')
+    verifyTheContextAttribute(contexts, 'dateEnd')
+    
+    return contexts
     
 def showNews(request):
     INTRO = "Estas son las noticias disponibles:<br>"
@@ -54,15 +73,13 @@ def showNews(request):
     contexts = getBodyContext(body)
     
     page = TOTAL_PAGE - contexts['lifespan']
-    number = contexts['parameters']['number']
+    number = contexts['parameters']['numberNews']
 
-    
     params = "?page=" + str(page)
     if(str(number).isdigit()):
         params += "&per_page=" + str(number)
     
     url = NEWS_API + params
-    
     response = requests.get(url)
     
     if response.status_code == requests.codes.ok:
@@ -83,7 +100,7 @@ def showCalls(request):
     contexts = getBodyContext(body)
     
     page = TOTAL_PAGE - contexts['lifespan']
-    number = contexts['parameters']['number']
+    number = contexts['parameters']['numberCalls']
 
     params = "?page=" + str(page)
     if(str(number).isdigit()):
@@ -102,6 +119,40 @@ def showCalls(request):
         return createResponseData(responseText)
         
     return createResponseData(CONNECTION_ERROR + ' las convocatorias.')
+    
+def showEvents(request):
+    INTRO = "Estos son los eventos disponibles:<br>"
+    
+    body = bodyToJSON(request.body)
+    contexts = getBodyContext(body)
+    
+    number = contexts['parameters']['numberEvents']
+    category = contexts['parameters']['category']
+    dateStart = contexts['parameters']['dateStart']
+    dateEnd = contexts['parameters']['dateEnd']
+    
+    url = EVENTS_API + category + '/' + dateStart + '/'
+    
+    if (dateEnd != ""):
+        url += dateEnd
+    else: 
+        url += str(number)
+    
+    response = requests.get(url)
+    
+    if response.status_code == requests.codes.ok:
+        results = response.json()
+        
+        titleEvents = "<ul>";
+        for value in results:
+            titleEvents += "<li><a target='_blank' href='" + value['permalink'] + "'>" + value['post_title'] + "</a></li><br>"
+        titleEvents += "</ul>"
+        
+        responseText = INTRO + titleEvents + 'Quieres mostrar los siguientes eventos?'
+            
+        return createResponseData(responseText)
+        
+    return createResponseData(CONNECTION_ERROR + ' los eventos.')
 
 def index(request):
     return HttpResponse("Webhook to NewsAgent.")
